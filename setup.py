7e6bd323b0be8af2055fd68a5446b930580b101f
YashMasane <masaneyash6@gmail.com>
2025-01-15 19:23:12 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yash Masane",
+    author_email="masaneyash6@gmail.com",
+    packages=find_packages()
+)
